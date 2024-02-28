f2f62cf57f0b11082b9abdc33d9654865fdb0faa
alphaprime8 <alphaprime8888@gmail.com>
2024-02-28 13:20:13 +0800

    init oos account success

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use std::cell::RefMut;
-use std::convert::identity;
+use std::fs;
+use std::mem::size_of;
 use std::str::FromStr;
 use openbook_dex::critbit::{SlabView};
 use openbook_dex::state::{MarketState, ToAlignedBytes};
@@ -7,8 +8,12 @@ use solana_program::{
     account_info::AccountInfo,
     pubkey::Pubkey,
 };
+use solana_program::instruction::Instruction;
 use solana_rpc_client::rpc_client::RpcClient;
 use solana_sdk::account::Account;
+use solana_sdk::bs58;
+use solana_sdk::signature::{Keypair, Signer};
+use solana_sdk::transaction::Transaction;
 
 const SOL_USDC_MARKET_ID: &str = "8BnEgHoWFysVcuFFX7QztDmzuH8r5ZFvyP3sYwn1XTh6";
 const OPENBOOK_V1_PROGRAM_ID: &str = "srmqPvymJeFKQ4zGQed1GFppgkRHL9kaELCbyksJtPX";
@@ -21,34 +26,74 @@ pub fn load_market() -> anyhow::Result<()>{
     let mut account = rpc_client.get_account(&SOL_USDC_MARKET_ID.parse().unwrap())?;
     let program_id_binding = OPENBOOK_V1_PROGRAM_ID.parse().unwrap();
     let market_account_binding = SOL_USDC_MARKET_ID.parse().unwrap();
-    // let binding = OPENBOOK_V1_PROGRAM_ID.parse().unwrap();
     let account_info = create_account_info_from_account(&mut account, &market_account_binding, &program_id_binding, false, false);
     let market_state = openbook_dex::state::MarketState::load(
         &account_info,
         &OPENBOOK_V1_PROGRAM_ID.parse().unwrap(),
         false,
     )?;
-    println!("got market state: {:?}", &market_state);
-
     let (bids_address, asks_address) = get_bids_asks_addresses(&market_state);
-
     let mut bids_account = rpc_client.get_account(&bids_address)?;
     let mut bids_info = create_account_info_from_account(&mut bids_account, &bids_address, &program_id_binding, false, false);
     let mut bids = market_state.load_bids_mut(&mut bids_info)?;
-
     loop {
         let node = bids.remove_max();
         match node {
-            Some(node) => {
-                println!("got node: {:?}", node);
+            Some(_node) => {
+                // println!("got node: {:?}", node);
             }
             None => {
                 break;
             }
         }
     }
+    let key_path = std::env::var("KEY_PATH").expect("RPC_URL is not set in .env file");
+    let keypair = read_keypair(&key_path);
+
+    let (orders_key, create_oos_account_ix) = create_dex_account(&rpc_client, &program_id_binding, &keypair.pubkey(), size_of::<openbook_dex::state::OpenOrders>())?;
+    let init_ix = openbook_dex::instruction::init_open_orders(
+        &program_id_binding,
+        &orders_key.pubkey(),
+        &keypair.pubkey(),
+        &market_account_binding,
+        None,
+    )?;
+    println!("got oos account: {:?}", orders_key.pubkey());
+
+    let mut instructions = Vec::new();
+    instructions.push(create_oos_account_ix);
+    instructions.push(init_ix);
+
+    let recent_hash = rpc_client.get_latest_blockhash()?;
+    let txn = Transaction::new_signed_with_payer(
+        &instructions,
+        Some(&keypair.pubkey()),
+        &[&orders_key, &keypair],
+        recent_hash,
+    );
+
+    let r = rpc_client.send_transaction(&txn);
+    println!("got results: {:?}", r);
 
     Ok(())
+}
+
+fn create_dex_account(
+    client: &RpcClient,
+    program_id: &Pubkey,
+    payer: &Pubkey,
+    unpadded_len: usize,
+) -> anyhow::Result<(Keypair, Instruction)> {
+    let len = unpadded_len + 12;
+    let key = Keypair::new();
+    let create_account_instr = solana_sdk::system_instruction::create_account(
+        payer,
+        &key.pubkey(),
+        client.get_minimum_balance_for_rent_exemption(len)?,
+        len as u64,
+        program_id,
+    );
+    Ok((key, create_account_instr))
 }
 
 fn get_bids_asks_addresses(market_state: &RefMut<MarketState>) -> (Pubkey, Pubkey) {
@@ -86,6 +131,19 @@ fn create_account_info_from_account<'a>(
         account.executable,
         account.rent_epoch,
     )
+}
+
+fn read_keypair(path: &String) -> Keypair {
+    let secret_string: String = fs::read_to_string(path).expect("Can't find key file");
+    let secret_bytes: Vec<u8> = match serde_json::from_str(&secret_string) {
+        Ok(bytes) => bytes,
+        Err(_) => match bs58::decode(&secret_string.trim()).into_vec() {
+            Ok(bytes) => bytes,
+            Err(_) => panic!("failed to load secret key from file"),
+        },
+    };
+    let keypair = Keypair::from_bytes(&secret_bytes).expect("failed to generate keypair from secret bytes");
+    keypair
 }
 
 #[cfg(test)]
