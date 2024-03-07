24f184ca08d55e7988e3429333cdddda39f635f2
alphaprime8 <alphaprime8888@gmail.com>
2024-03-07 12:32:24 +0800

    highly redundant convenience function

--- src/ixs/combo.rs
+++ src/ixs/combo.rs
@@ -17,6 +17,19 @@ pub fn combo_cancel_settle_place(
 ) -> anyhow::Result<()> {
 
     let mut instructions = Vec::new();
+    let r = ob_client.rpc_client.get_recent_prioritization_fees(&[]).unwrap();
+    let mut max_fee = 1;
+    for f in r {
+        if f.prioritization_fee > max_fee {
+            max_fee = f.prioritization_fee;
+        }
+    }
+
+    let budget_ix = ComputeBudgetInstruction::set_compute_unit_limit(1_000_000);
+    let fee_ix = ComputeBudgetInstruction::set_compute_unit_price(max_fee);
+    instructions.push(budget_ix);
+    instructions.push(fee_ix);
+
     let ixs = cancel_all_limit_orders(&mut ob_client, false)?;
     if let Some(ixs) = ixs {
         instructions.extend(ixs);
@@ -39,7 +52,7 @@ pub fn combo_cancel_settle_place(
     let mut config = RpcSendTransactionConfig::default();
     config.skip_preflight = false;
     let r = ob_client.rpc_client.send_transaction_with_config(&txn, config);
-    tracing::debug!("got results: {:?}", r);
+    tracing::info!("got results: {:?}", r);
 
     Ok(())
 }
@@ -72,6 +85,52 @@ pub fn combo_cancel_settle_place_bid(
     let ixs = settle_balance(&mut ob_client, false)?.unwrap();
     instructions.extend(ixs);
     let ixs = place_limit_order(&mut ob_client, target_size_usdc_bid, Side::Bid, 0., false, bid_price_jlp_usdc)?.unwrap();
+    instructions.extend(ixs);
+
+    let recent_hash = ob_client.rpc_client.get_latest_blockhash()?;
+    let txn = Transaction::new_signed_with_payer(
+        &instructions,
+        Some(&ob_client.keypair.pubkey()),
+        &[&ob_client.keypair],
+        recent_hash,
+    );
+
+    let mut config = RpcSendTransactionConfig::default();
+    config.skip_preflight = false;
+    let r = ob_client.rpc_client.send_transaction_with_config(&txn, config);
+    tracing::info!("got results: {:?}", r);
+
+    Ok(())
+}
+pub fn combo_cancel_settle_place_ask(
+    mut ob_client: &mut ObClient,
+    target_size_usdc_ask: f64,
+    ask_price_jlp_usdc: f64,
+) -> anyhow::Result<()> {
+
+    let mut instructions = Vec::new();
+
+
+    let r = ob_client.rpc_client.get_recent_prioritization_fees(&[]).unwrap();
+    let mut max_fee = 1;
+    for f in r {
+        if f.prioritization_fee > max_fee {
+            max_fee = f.prioritization_fee;
+        }
+    }
+
+    let budget_ix = ComputeBudgetInstruction::set_compute_unit_limit(800_000);
+    let fee_ix = ComputeBudgetInstruction::set_compute_unit_price(max_fee);
+    instructions.push(budget_ix);
+    instructions.push(fee_ix);
+
+    let ixs = cancel_all_limit_orders(&mut ob_client, false)?;
+    if let Some(ixs) = ixs {
+        instructions.extend(ixs);
+    }
+    let ixs = settle_balance(&mut ob_client, false)?.unwrap();
+    instructions.extend(ixs);
+    let ixs = place_limit_order(&mut ob_client, target_size_usdc_ask, Side::Ask, 0., false, ask_price_jlp_usdc)?.unwrap();
     instructions.extend(ixs);
 
     let recent_hash = ob_client.rpc_client.get_latest_blockhash()?;
